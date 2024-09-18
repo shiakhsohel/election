e56e1e5e9aae6c5325ebdacf37a4a28b44e79013
Mohammed Samiuddin <66519042+Ms3314@users.noreply.github.com>
2024-09-18 19:05:50 +0530

    Update userRoutes.js
    update 1

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -5,6 +5,9 @@ const userRouter = express.Router();
 const userController = require('../src/v1/voter/controllers/userController');
 
 // Register a new user
+// Register a new user
 userRouter.post('/register', userController.registerUser);
+userRouter.post('/login', userController.loginUser);
 
 module.exports = userRouter;
+
